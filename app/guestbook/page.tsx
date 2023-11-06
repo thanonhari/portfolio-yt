93f08a35d035cecb142c8ae8195d4325d77d27ae
thanonhari karnbanchorn <thanonhari@gmail.com>
2023-11-06 13:53:22 +0700

    guest form

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -1,5 +1,5 @@
 import Form from "../components/Form";
-import prisma from "../db";
+import  prisma  from "../db";
 
 async function getEntries() {
   const data = await prisma.guestbook.findMany({
@@ -12,24 +12,22 @@ async function getEntries() {
   return data;
 }
 
-export const revalidte = 60;
+export const revalidate = 60;
 
 export default async function Guestbook() {
   const data = await getEntries();
-
   return (
-    <div
-      className="divied-y divide-gray-200 dark:divide-gray-700
-        "
-    >
+    <div className="divide-y divide-gray-200 dark:divide-gray-700">
       <div className="space-y-2 pt-6 pb-8 md:space-y-5">
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
           Guestbook
         </h1>
       </div>
+
       <div className="w-full">
         <div className="max-w-[500px] mx-auto mt-8">
           <Form />
+
           <div className="flex flex-col space-y-2">
             {data.map((entry) => (
               <div key={entry.id} className="w-full text-sm break-words">
